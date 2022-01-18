779c5a235e839de04a912d80033e0b55a40a7d9e
Ramil Galikeev <galikeevramil95@gmail.com>
2022-01-18 15:11:59 +0500

    added complete deletion todo

--- src/components/todoList/TodoList.js
+++ src/components/todoList/TodoList.js
@@ -1,22 +1,35 @@
-import { List, ListItem, ListItemSecondaryAction, ListItemText, Checkbox, IconButton} from "@mui/material";
+import { List, ListItem, ListItemSecondaryAction, ListItemText, Checkbox, IconButton, Button} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 
-const TodoList = ({todos, onChangeStatusTodo, onChangeChexbox}) => {
+const TodoList = ({todos, onChangeStatusTodo, onAllDeleteTodo}) => {
 
     return (
-        <List>
-            {todos.map((todo, i) => (
-                <ListItem key={todo.id} dense button>
-                    {todo.status === 'completed' || todo.status === 'deleted' ? null : <Checkbox 
-                        tabIndex={-1} 
-                        disableRipple
-                        onChange={() => {onChangeStatusTodo(todo.id, 'completed')}}
-                    />}
+        <List style={{'margin' : '20px 0 20px 0'}}>
+            {todos.length === 0 ? <div style={{'textAlign': 'center'}}>Задач пока нет</div> : todos.map((todo, i) => (
+                <ListItem 
+                    key={todo.id} 
+                    dense 
+                    button
+                >
+                    {todo.status === 'completed' || todo.status === 'deleted' ? 
+                        null : 
+                        <Checkbox 
+                            tabIndex={-1} 
+                            disableRipple
+                            onChange={() => {onChangeStatusTodo(todo.id, 'completed')}}/>}
                     <ListItemText primary={todo.title}/>
                     <ListItemSecondaryAction>
-                        {todo.status === 'deleted' ? null : <IconButton 
-                            aria-label="delete"
-                            onClick={() => {onChangeStatusTodo(todo.id, 'deleted')}}>
+                        {todo.status === 'deleted' ? 
+                            <Button 
+                                variant="contained" 
+                                onClick={() => onAllDeleteTodo(todo.id)}
+                            >
+                                удалить полностью
+                            </Button> : 
+                            <IconButton 
+                                aria-label="delete"
+                                onClick={() => {onChangeStatusTodo(todo.id, 'deleted')}}
+                            >
                             <DeleteIcon/>
                         </IconButton>}
                     </ListItemSecondaryAction>
